02150cabaa233cb4d678d494e9a51c2a13d1a4c4
LuyenDH <luyendh@fpt.edu.vn>
2019-09-12 19:45:47 +0700

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import {RouterModule, Routes} from '@angular/router';
+import {HeroListComponent} from './module/hero/hero-list/hero-list.component';
+import {ProductListComponent} from './module/product/product-list/product-list.component';
+
+const routes: Routes = [
+  {
+    path: 'hero',
+    component: HeroListComponent
+  },
+  {
+    path: 'product',
+    component: ProductListComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    HeroListComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/module/hero/hero.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroListComponent } from './hero-list/hero-list.component';
+
+
+
+@NgModule({
+  declarations: [HeroListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeroModule { }
